85a65d1c99c97e47ec82a7371043c2e6cbc31444
Bao Mai <maiduybao@gmail.com>
2017-11-01 11:20:14 -0700

    partitioning and run partitions in parallel

--- test/test.js
+++ test/test.js
@@ -9,19 +9,16 @@ describe("Filter tests", function() {
 
         const filter = new Filter();
 
-        it("Should detect a bad word and return a boolean value", function (done) {
-            assert.equal(true, filter.isProfane("ash0le"));
-            done();
+        it("Should detect a bad word and return a boolean value", async function () {
+            assert.equal(true, await filter.isProfane("ash0le"));
         });
 
-        it("Should return false when no bad word is detected", function (done) {
-            assert.equal(false, filter.isProfane("wife"));
-            done();
+        it("Should return false when no bad word is detected", async function () {
+            assert.equal(false, await filter.isProfane("wife"));
         });
 
-        it("Should be able to detect a bad word in a sentence", function (done) {
-            assert.equal(true, filter.isProfane("that person is an ash0le"));
-            done();
+        it("Should be able to detect a bad word in a sentence", async function () {
+            assert.equal(true, await filter.isProfane("that person is an ash0le"));
         });
 
     });
@@ -29,23 +26,19 @@ describe("Filter tests", function() {
     describe('#clean', function () {
         const filter = new Filter();
 
-        it("Should replace a bad word within a sentence with asterisks except for first letter in non strict mode (******)", function (done) {
-            filter.clean("Don't be an ash0le").should.equal("Don't be an ******");
-            done();
+        it("Should replace a bad word within a sentence with asterisks except for first letter in non strict mode (******)", async function () {
+            (await filter.clean("Don't be an ash0le")).should.equal("Don't be an ******");
         });
-        it("Should replace a bad word within a sentence asterisks (******)", function (done) {
-            filter.clean("Don't be an ash0le").should.equal("Don't be an ******");
-            done();
+        it("Should replace a bad word within a sentence asterisks (******)", async function () {
+            (await filter.clean("Don't be an ash0le")).should.equal("Don't be an ******");
         });
 
-        it("Should replace multiple instances of any bad words within a sentence asterisks (******)", function (done) {
-            filter.clean("cnts ash0le knob xxx").should.equal("**** ****** **** ***");
-            done();
+        it("Should replace multiple instances of any bad words within a sentence asterisks (******)", async function () {
+            (await filter.clean("cnts ash0le knob xxx")).should.equal("**** ****** **** ***");
         });
 
-        it("Should not replace anything within a sentence if there are no bad words", function (done) {
-            filter.clean("The cat ran fast").should.equal("The cat ran fast");
-            done();
+        it("Should not replace anything within a sentence if there are no bad words", async function () {
+            (await filter.clean("The cat ran fast")).should.equal("The cat ran fast");
         });
     });
 
